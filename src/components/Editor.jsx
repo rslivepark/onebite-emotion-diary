6dcc27b4a2c962c7e53200f4749313e9cfe60a66
rslivepark <rslivepark@gmail.com>
2024-06-30 14:12:44 +0900

    일기 작성 페이지 추가

---
+++ src/components/Editor.jsx
@@ -0,0 +1,128 @@
+import React, { useState } from 'react';
+import './Editor.css';
+import EmotionItem from './EmotionItem';
+import Button from './Button';
+import { useNavigate } from 'react-router-dom';
+
+const emotionList = [
+  {
+    emotionId: 1,
+    emotionName: '완전좋음',
+  },
+  {
+    emotionId: 2,
+    emotionName: '좋음',
+  },
+  {
+    emotionId: 3,
+    emotionName: '그냥저냥',
+  },
+  {
+    emotionId: 4,
+    emotionName: '안좋음',
+  },
+  {
+    emotionId: 5,
+    emotionName: '완전별로',
+  },
+];
+
+const getStringedDate = (targetDate) => {
+  // YYYY-MM-DD
+  let year = targetDate.getFullYear();
+  let month = targetDate.getMonth() + 1;
+  let date = targetDate.getDate();
+
+  if (month < 10) {
+    month = `0${month}`;
+  }
+
+  if (date < 10) {
+    date = `0${date}`;
+  }
+  return `${year}-${month}-${date}`;
+};
+
+const Editor = ({ onSubmit }) => {
+  const nav = useNavigate();
+
+  const [input, setInput] = useState({
+    createdDate: new Date(),
+    emotionId: 3,
+    content: '',
+  });
+
+  const onChangeInput = (e) => {
+    // console.log(e.target.name); // 어떤 요소에 입력이 들어온건지
+    // console.log(e.target.value); // 입력된 값이 무엇인지?
+
+    let name = e.target.name;
+    let value = e.target.value;
+
+    if (name === 'createdDate') {
+      value = new Date(value);
+    }
+
+    setInput({
+      ...input,
+      [name]: value,
+    });
+  };
+
+  const onClickSubmitButton = () => {
+    onSubmit(input);
+  };
+
+  return (
+    <div className='Editor'>
+      <section className='date_section'>
+        <h4>오늘의 날짜</h4>
+        <input
+          name='createdDate'
+          onChange={onChangeInput}
+          value={getStringedDate(input.createdDate)}
+          type='date'
+        />
+      </section>
+      <section className='emotion_section'>
+        <h4>오늘의 감정</h4>
+        <div className='emotion_list_wrapper'>
+          {emotionList.map((item) => (
+            <EmotionItem
+              onClick={() =>
+                onChangeInput({
+                  target: {
+                    name: 'emotionId',
+                    value: item.emotionId,
+                  },
+                })
+              }
+              key={item.emotionId}
+              {...item}
+              isSelected={item.emotionId === input.emotionId}
+            />
+          ))}
+        </div>
+      </section>
+      <section className='content_section'>
+        <h4>오늘의 일기</h4>
+        <textarea
+          name='content'
+          value={input.content}
+          onChange={onChangeInput}
+          placeholder='오늘의 기분은 어땠나요?'
+        ></textarea>
+      </section>
+      <section className='button_section'>
+        <Button onClick={() => nav(-1)} text={'작성취소'} />
+        <Button
+          onClick={onClickSubmitButton}
+          text={'작성완료'}
+          type={'POSITIVE'}
+        />
+      </section>
+    </div>
+  );
+};
+
+export default Editor;
--- src/pages/New.jsx
+++ src/pages/New.jsx
@@ -1,8 +1,35 @@
 import React from 'react';
+import Header from '../components/Header';
 import Button from '../components/Button';
+import Editor from '../components/Editor';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleLeft } from '@fortawesome/free-solid-svg-icons';
+import { useNavigate } from 'react-router-dom';
+import { useContext } from 'react';
+import { DiaryDispatchContext } from '../App';
 
 const New = () => {
-  return <div className='New'>Write diary..</div>;
+  const { onCreate } = useContext(DiaryDispatchContext);
+  const nav = useNavigate();
+  const onSubmit = (input) => {
+    onCreate(input.createdDate.getTime(), input.emotionId, input.content);
+    nav('/', { replace: true });
+  };
+
+  return (
+    <div className='New'>
+      <Header
+        title={'일기쓰기'}
+        leftChild={
+          <Button
+            onClick={() => nav(-1)}
+            text={<FontAwesomeIcon icon={faAngleLeft} />}
+          />
+        }
+      />
+      <Editor onSubmit={onSubmit} />
+    </div>
+  );
 };
 
 export default New;
