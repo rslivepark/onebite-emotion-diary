230f0d073dcaa4af0b8296a9e3605d8ee2d0ef5a
rslivepark <rslivepark@gmail.com>
2024-06-30 21:08:49 +0900

    로컬스토리지 데이터 저장

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,78 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
-import { useReducer, useRef, createContext } from 'react';
+import { useReducer, useRef, createContext, useEffect, useState } from 'react';
 
 import Home from './pages/Home';
 import New from './pages/New';
 import Diary from './pages/Diary';
 import Notfound from './pages/Notfound';
 import Edit from './pages/Edit';
-import Header from './components/Header';
-import Button from './components/Button';
-
-import { getEmotionImage } from './util/get-emotion-image';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
-
-const mockData = [
-  {
-    id: 1,
-    createdDate: new Date('2024-06-29').getTime(),
-    emotionId: 1,
-    content: '1st diary...',
-  },
-  {
-    id: 2,
-    createdDate: new Date('2024-05-29').getTime(),
-    emotionId: 2,
-    content: '2nd diary...',
-  },
-  {
-    id: 3,
-    createdDate: new Date('2024-07-29').getTime(),
-    emotionId: 3,
-    content: '3rd diary...',
-  },
-  {
-    id: 4,
-    createdDate: new Date('2024-06-23').getTime(),
-    emotionId: 3,
-    content: '4th diary...',
-  },
-];
 
 function reducer(state, action) {
+  let nextState;
+
   switch (action.type) {
-    case 'CREATE':
-      return [...state];
-    case 'UPDATE':
-      return state.map((item) =>
+    case 'INIT':
+      return action.data;
+    case 'CREATE': {
+      nextState = [action.data, ...state];
+      break;
+    }
+    case 'UPDATE': {
+      nextState = state.map((item) =>
         String(item.id) === String(action.data.id) ? action.data : item
       );
-    case 'DELETE':
-      return state.filter((item) => String(item.id) !== String(action.data.id));
+      break;
+    }
+    case 'DELETE': {
+      nextState = state.filter(
+        (item) => String(item.id) !== String(action.data.id)
+      );
+      break;
+    }
     default:
       return state;
   }
+  localStorage.setItem('diary', JSON.stringify(nextState));
+  return nextState;
 }
 
 export const DiaryStateContext = createContext();
 export const DiaryDispatchContext = createContext();
 
 function App() {
-  const [data, dispatch] = useReducer(reducer, mockData);
-  const idRef = useRef(4);
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, dispatch] = useReducer(reducer, []);
+  const idRef = useRef(0);
+
+  useEffect(() => {
+    const storedData = localStorage.getItem('diary');
+    if (!storedData) {
+      setIsLoading(false);
+      return;
+    }
+    const parsedData = JSON.parse(storedData);
+
+    if (!Array.isArray(parsedData)) {
+      setIsLoading(false);
+      return;
+    }
+
+    let maxId = 0;
+    parsedData.forEach((item) => {
+      if (Number(item.id) > maxId) {
+        maxId = Number(item.id);
+      }
+    });
+
+    idRef.current = maxId + 1;
+
+    dispatch({
+      type: 'INIT',
+      data: parsedData,
+    });
+    setIsLoading(false);
+  }, []);
 
   // 일기 쓰기
   const onCreate = (createdDate, emotionId, content) => {
@@ -99,6 +110,10 @@ function App() {
       },
     });
   };
+
+  if (isLoading) {
+    return <div>데이터 로딩중...</div>;
+  }
 
   return (
     <>
